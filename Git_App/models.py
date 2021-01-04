c16fa99a3ef3a3e2f1dacc2587a34ef7135d6df6
srinivasrao452 <srinivasrao452@gmail.com>
2021-01-04 20:21:36 +0530

    model update

--- Git_App/models.py
+++ Git_App/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Emp(models.Model):
-  eno = models.CharField(max_length=30)
+  eno = models.IntegerField()
+  ename = models.CharField(max_length=30)
